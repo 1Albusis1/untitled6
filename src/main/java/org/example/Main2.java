31462db92bfcefc900a21d0f64468129386cc6c3
Альберт <alikmanasow@gmail.com>
2023-11-10 22:07:53 +0300

    ублюдки

--- src/main/java/org/example/Main2.java
+++ src/main/java/org/example/Main2.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main2 {
     public static void main(String[] args) {
-        System.out.println("Салам пацантре");
+        System.out.println("Пошли все нахуй");
     }
 }
